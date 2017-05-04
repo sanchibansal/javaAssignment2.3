fb880ed12e57c438d7700928a8b019bfb3f9f1a4
sanchibansal <bansalsanchi77.sb@gmail.com>
2017-05-04 20:54:04 +0530

    code

--- Highest.java
+++ Highest.java
@@ -1,26 +1,26 @@
 import java.util.*;
-class Highest
-{
-	public static void main(String args[])
-{
-	Scanner g= new Scanner(System.in);
-	System.out.println("enter three numbers.");
-	int a=g.nextInt();
-	int b=g.nextInt();		//input numbers through scanner
-	int c=g.nextInt();
-	if(c>b)
-	{
-		if(a>c)						//prints a if a is greater than c
-		{System.out.println(a +" is the highest number.");}
-		if(a<c)						///prints c if c is greater than a
-		{System.out.println(c+" is the highest number.");}
-	}
-	if(b>c)
-	{
-		if(a<b)						//prints b if b is greater than a
-		{ System.out.println(b+" is the largest number.");}
-		if(a>b)						//prints a if a is greater than b
-		{ System.out.println(a+" is the largest number.");}
-	}
-}
+class Highest{
+    public static void main(String args[]){
+    Scanner g= new Scanner(System.in);
+    System.out.println("enter three numbers.");
+    int a=g.nextInt();
+    int b=g.nextInt();		//input numbers through scanner
+    int c=g.nextInt();
+    if(c>b){
+	if(a>c){						//prints a if a is greater than c
+	System.out.println(a +" is the highest number.");
+        }
+	if(a<c){						//prints c if c is greater than a
+	System.out.println(c+" is the highest number.");
+        }
+    }
+    if(b>c){
+	if(a<b){						//prints b if b is greater than a
+	System.out.println(b+" is the largest number.");
+        }
+	if(a>b){						//prints a if a is greater than b
+	System.out.println(a+" is the largest number.");
+        }
+    }
+    }
 }
